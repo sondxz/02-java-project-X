bf4f42952895bea80cdcabc4c785d5e420b4bad6
sondxz <hoangsonnguyen204@gmail.com>
2026-01-20 15:24:55 +0700

    init: Chapter 03 video 009

--- src/main/java/vn/hoangson/todo/HelloController.java
+++ src/main/java/vn/hoangson/todo/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello Spring Boot!";
+        return "Hello Spring Boot! abc";
     }
     
 }
