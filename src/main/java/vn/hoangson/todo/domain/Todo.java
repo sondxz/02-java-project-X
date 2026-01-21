2bbf78516243116f44b88234efd3b4c976438bce
sondxz <hoangsonnguyen204@gmail.com>
2026-01-21 09:28:17 +0700

    init: Chapter 04 video 004 + 005

---
+++ src/main/java/vn/hoangson/todo/domain/Todo.java
@@ -0,0 +1,19 @@
+package vn.hoangson.todo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    String title;
+    Boolean isCompleted;
+
+}
