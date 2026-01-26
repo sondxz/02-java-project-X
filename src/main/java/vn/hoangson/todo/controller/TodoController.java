d7251188d7e3e412e6a14d979b4619c011a6cac4
sondxz <hoangsonnguyen204@gmail.com>
2026-01-26 22:00:01 +0700

    feat: Chapter 05 video 012

--- src/main/java/vn/hoangson/todo/controller/TodoController.java
+++ src/main/java/vn/hoangson/todo/controller/TodoController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +40,7 @@ public class TodoController {
         return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
     }
 
-    @GetMapping("/update-todo")
+    @PutMapping("/todos")
     public String postUpdateTodo() {
         this.todoService.handleUpdateTodo();
         return "update todo endpoint";
