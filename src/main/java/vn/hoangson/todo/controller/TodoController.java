41c4ec23886819868b66d1c867848e73629938b6
sondxz <hoangsonnguyen204@gmail.com>
2026-01-26 22:13:09 +0700

    feat: Chapter 05 video 015

--- src/main/java/vn/hoangson/todo/controller/TodoController.java
+++ src/main/java/vn/hoangson/todo/controller/TodoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -46,9 +47,9 @@ public class TodoController {
         return ResponseEntity.ok().body(" update successfully");
     }
 
-    @GetMapping("/delete-todo")
-    public String postDeleteTodo() {
-        this.todoService.handleDeleteTodo();
+    @DeleteMapping("/todos")
+    public String deleteTodo() {
+        // this.todoService.handleDeleteTodo();
         return " delete todo endpoint";
     }
 
