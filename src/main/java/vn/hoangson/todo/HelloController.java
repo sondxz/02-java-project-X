2b6f940433eb7c4fe8d82959c676af9489d289ef
sondxz <hoangsonnguyen204@gmail.com>
2026-01-26 20:43:50 +0700

    feat: Chapter 05 video 006

--- src/main/java/vn/hoangson/todo/HelloController.java
+++ src/main/java/vn/hoangson/todo/HelloController.java
@@ -1,14 +1,24 @@
 package vn.hoangson.todo;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoangson.todo.domain.Todo;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 public class HelloController {
     @GetMapping("/")
-    public String index() {
-        return "Hello Spring Boot! abc";
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok().body("Hello Spring Boot!");
+    }
+    
+    @GetMapping("/hoidanit")
+    public ResponseEntity<Todo> hoidanit() {
+        Todo todo = new Todo("Hỏi dân IT", false);
+        return ResponseEntity.ok().body(todo);
     }
     
 }
