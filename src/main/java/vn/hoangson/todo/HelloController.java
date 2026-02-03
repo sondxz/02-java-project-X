2ac69a258406475d9ed0f44510ece0fa06454031
sondxz <hoangsonnguyen204@gmail.com>
2026-02-03 20:57:16 +0700

    feat: Chapter 06 video 010 + 012

--- src/main/java/vn/hoangson/todo/HelloController.java
+++ src/main/java/vn/hoangson/todo/HelloController.java
@@ -2,23 +2,40 @@ package vn.hoangson.todo;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import vn.hoangson.todo.domain.Todo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import vn.hoangson.todo.domain.Todo;
+import vn.hoangson.todo.domain.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @RestController
 public class HelloController {
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @GetMapping("/")
-    public ResponseEntity<String> index() {
-        return ResponseEntity.ok().body("Hello Spring Boot!");
+    public ResponseEntity<String> index() throws Exception {
+        // json => object(frontend send data to backend)
+        String json = """
+                {
+                "name": "eric",
+                "email": "hoidanit@gmail.com"
+                }""";
+        User user = objectMapper.readValue(json, User.class);
+
+        User eric = new User(null, "eric", "hoidanit@gmail.com");
+        String jsonOutput = objectMapper.writeValueAsString(eric);
+
+        return ResponseEntity.ok().body(jsonOutput);
     }
-    
+
     @GetMapping("/hoidanit")
     public ResponseEntity<Todo> hoidanit() {
         Todo todo = new Todo("Hỏi dân IT", false);
         return ResponseEntity.ok().body(todo);
     }
-    
+
 }
